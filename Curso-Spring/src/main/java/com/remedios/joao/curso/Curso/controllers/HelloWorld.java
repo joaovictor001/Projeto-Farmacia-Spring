480278af89bbb0fcec5f34fdeb0b096421248a7b
joaovictor001 <74721656+joaovictor001@users.noreply.github.com>
2024-05-16 23:14:31 -0300

    fist commit

---
+++ Curso-Spring/src/main/java/com/remedios/joao/curso/Curso/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.remedios.joao.curso.Curso.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloWorld {
+	
+	@GetMapping
+	public String olaMundo() {
+		return "Olá Mundo";
+	}
+}
---
+++ Curso-Spring/src/test/java/com/remedios/joao/curso/Curso/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios.joao.curso.Curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
