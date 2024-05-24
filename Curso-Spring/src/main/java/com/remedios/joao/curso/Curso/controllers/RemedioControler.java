96946f2f7bbcd2fd94fabd6d326838a2beb76757
joaovictor001 <74721656+joaovictor001@users.noreply.github.com>
2024-05-23 21:32:08 -0300

    ResponseEntity

--- Curso-Spring/src/main/java/com/remedios/joao/curso/Curso/controllers/RemedioControler.java
+++ Curso-Spring/src/main/java/com/remedios/joao/curso/Curso/controllers/RemedioControler.java
@@ -1,6 +1,7 @@
 package com.remedios.joao.curso.Curso.controllers;
 
 import com.remedios.joao.curso.Curso.controllers.entites.remedio.dtos.DadosAtualizarRemedio;
+import com.remedios.joao.curso.Curso.controllers.entites.remedio.dtos.DadosDetalhamentoRemedios;
 import com.remedios.joao.curso.Curso.controllers.entites.remedio.dtos.OutDadosCadastroRemedio;
 import com.remedios.joao.curso.Curso.controllers.entites.remedio.dtos.InDadosCadastroRemedio;
 import com.remedios.joao.curso.Curso.controllers.entites.remedio.Remedio;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.PublicKey;
@@ -17,6 +19,8 @@ import java.util.List;
 
 //@RestController indica para o spring que essa é um aclase sping e precisa ser inicializada.
 //@RequestMapping setar o meu endpoint da Api.
+
+//Link Swagger => http://localhost:8081/swagger-ui/index.html#/
 @RestController
 @RequestMapping("/remedios")
 @Tag(name = "remedios_api")
@@ -39,40 +43,50 @@ public class RemedioControler {
     }
 
     @GetMapping
-    public List<OutDadosCadastroRemedio> listar(){
+    public  ResponseEntity<List<OutDadosCadastroRemedio>> listar(){
         // A função findAll(boolean)(true or false) cria automatico para campos boolean so precisamos
-        return repository.findAllByAtivoTrue().stream().map(OutDadosCadastroRemedio::new).toList();
+        var lista =  repository.findAllByAtivoTrue().stream().map(OutDadosCadastroRemedio::new).toList();
+        return ResponseEntity.ok(lista);
     }
 
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizarRemedio dados){
+    public ResponseEntity<DadosDetalhamentoRemedios> atualizar(@RequestBody @Valid DadosAtualizarRemedio dados){
         var remedio = repository.getReferenceById(dados.id());
         remedio.atualizarInformacoes(dados);
+
+        return  ResponseEntity.ok(new DadosDetalhamentoRemedios(remedio));
     }
 
 
 
     @DeleteMapping("/{id}")
     @Transactional
-    public  void excluir(@PathVariable long id ){
+    public ResponseEntity<Void> excluir(@PathVariable long id ){
         repository.deleteById(id);
+
+        return  ResponseEntity.noContent().build();
+
     }
 
     @DeleteMapping("inativar/{id}")
     @Transactional
-    public  void  inativar(@PathVariable long id){
+    public ResponseEntity<Void> inativar(@PathVariable long id){
         var remedio = repository.getReferenceById(id);
         remedio.inativar();
+
+        return ResponseEntity.noContent().build();
     }
 
 
     @PutMapping("ativar/{id}")
     @Transactional
-    public void Reativar(@PathVariable long id){
+    public ResponseEntity<Void> Reativar(@PathVariable long id){
         var ativacao = repository.getReferenceById(id);
         ativacao.ativar();
+
+        return ResponseEntity.noContent().build();
     }
 
 
