57ecad4f724ec7ac2d8d3fe2a4aeddd82e62c41a
Nicolas Seroiska <nicolas.seroiska@digiage.com.br>
2024-05-31 14:11:12 -0300

    UserDetailService

--- Curso-Spring/src/main/java/com/remedios/joao/curso/Curso/Secutiry/CustomUserDetailService.java
+++ Curso-Spring/src/main/java/com/remedios/joao/curso/Curso/Secutiry/CustomUserDetailService.java
@@ -1,2 +1,25 @@
-package com.remedios.joao.curso.Curso.Secutiry;public class CustomUserDetailService {
+package com.remedios.joao.curso.Curso.Secutiry;
+
+import com.remedios.joao.curso.Curso.entites.usuario.Repository.UsuarioRepository;
+import com.remedios.joao.curso.Curso.entites.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario user = (Usuario) this.usuarioRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPssword(),new ArrayList<>());
+    }
 }
